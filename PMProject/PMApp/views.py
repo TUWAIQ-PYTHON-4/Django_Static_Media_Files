099aba0ebe460f54e10139b63f9408d7921b6b7e
t h <taif@gmail.com>
2022-05-25 16:58:12 +0300

    solved lab

---
+++ PMProject/PMApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Project
+
+
+def Index_viwes(request):
+    image=Project.objects.all()
+    return render(request,"base.html",{'image':image })
